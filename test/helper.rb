da377a39cebea4ac875c29dcc5f0ff8cb91eb454
Ian Heggie <ian@heggie.biz>
2010-01-28 16:29:31 +1100

    Initial commit to health_check.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'health_check'
+
+class Test::Unit::TestCase
+end
