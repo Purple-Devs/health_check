7aa4bd98e1b7b68d401aec4e058544d994de9bf2
Ian Heggie <ian@heggie.biz>
2013-03-05 00:47:25 +1100

    Add .format to routes, and restrict to http GET's only

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 if defined?(HealthCheck::Engine)
   Rails.application.routes.draw do
-    match 'health_check', :to => 'health_check/health_check#index'
-    match 'health_check/:checks', :to => 'health_check/health_check#check'
+    get 'health_check', :to => 'health_check/health_check#index'
+    get 'health_check.:format', :to => 'health_check/health_check#index'
+    get 'health_check/:checks', :to => 'health_check/health_check#index'
+    get 'health_check/:checks.:format', :to => 'health_check/health_check#index'
   end
 end
--- lib/health_check/add_23_routes.rb
+++ lib/health_check/add_23_routes.rb
@@ -6,14 +6,19 @@ module ActionController
   module Routing #:nodoc:
     class RouteSet #:nodoc:
       alias_method :draw_without_health_check_routes, :draw
+
       def draw
-	draw_without_health_check_routes do |map|
-	  map.connect 'health_check',
-	    :controller => 'health_check/health_check', :action => 'index'
-	  map.connect 'health_check/:checks',
-	    :controller => 'health_check/health_check', :action => 'check'
-	  yield map
-	end
+        draw_without_health_check_routes do |map|
+          map.connect 'health_check',
+                      :controller => 'health_check/health_check', :action => 'index'
+          map.connect 'health_check.:format',
+                      :controller => 'health_check/health_check', :action => 'index'
+          map.connect 'health_check/:checks',
+                      :controller => 'health_check/health_check', :action => 'index'
+          map.connect 'health_check/:checks.:format',
+                      :controller => 'health_check/health_check', :action => 'index'
+          yield map
+        end
       end
     end
   end
