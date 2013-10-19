e205558dcf379727c61fba32ad758b106a1ad912
Ian Heggie <ian@heggie.biz>
2013-10-19 21:40:02 +1100

    Revert to Rails.application.draw

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 if defined?(HealthCheck::Engine)
   #Rails.application.routes.draw do
   unless HealthCheck::Engine.routes_manually_defined
-    HealthCheck::Engine.routes.draw do
+    # Ifmountable use: HealthCheck::Engine.routes.draw do
+    Rails.application.draw do
       health_check_routes(false)
     end
   end
