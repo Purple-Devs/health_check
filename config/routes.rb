894b46bff050465da6891300a19d313f1d64eb71
Ian Heggie <ian@heggie.biz>
2013-10-20 21:11:16 +1100

    Correct routes.draw

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 if defined?(HealthCheck::Engine)
-  #Rails.application.routes.draw do
   unless HealthCheck::Engine.routes_manually_defined
     # Ifmountable use: HealthCheck::Engine.routes.draw do
-    Rails.application.draw do
+    Rails.application.routes.draw do
       health_check_routes(false)
     end
   end
