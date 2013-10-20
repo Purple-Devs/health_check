3f9dd6af4e73d6380373107a7579990853511bcf
Ian Heggie <ian@heggie.biz>
2013-10-20 18:45:56 +1100

    Simplify route

--- lib/health_check/health_check_routes.rb
+++ lib/health_check/health_check_routes.rb
@@ -5,10 +5,7 @@ if defined?(HealthCheck::Engine)
       def health_check_routes
         unless HealthCheck::Engine.routes_defined
           HealthCheck::Engine.routes_defined = true
-          get 'health_check', :to => 'health_check/health_check#index'
-          get 'health_check.:format', :to => 'health_check/health_check#index'
-          get 'health_check/:checks', :to => 'health_check/health_check#index'
-          get 'health_check/:checks.:format', :to => 'health_check/health_check#index'
+          get 'health_check(/:checks)(.:format)', :to => 'health_check/health_check#index'
         end
       end
     end
