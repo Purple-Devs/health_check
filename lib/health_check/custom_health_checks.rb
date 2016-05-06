1195eb7ce72dad713647e27c644b2f103b2b0d0a
Filip <filip.haftek@airhelp.com>
2016-05-06 14:22:41 +0200

    Code review fixes

--- lib/health_check/custom_health_checks.rb
+++ lib/health_check/custom_health_checks.rb
@@ -3,7 +3,7 @@ module HealthCheck
 
     def self.check_sidekiq_redis
       Sidekiq.redis { |r| "" if r.ping == "PONG" }
-    rescue => e
+    rescue Exception => e
        create_error 'sidekiq-redis', e.message
     end
 
@@ -14,11 +14,10 @@ module HealthCheck
     end
 
     def self.check_s3
-      s3Client = s3_client
-      return create_error 's3', "Could not connect to aws" if s3Client.nil?
+      return create_error 's3', "Could not connect to aws" if aws_s3_client.nil?
       HealthCheck.buckets.each do |bucket|
-        s3Client.put_object(bucket: bucket,key: 'FOO', body: 'BAR')
-        if !s3Client.get_object(bucket: bucket,key: 'FOO').successful?
+        aws_s3_client.put_object(bucket: bucket, key: 'FOO', body: 'BAR')
+        unless aws_s3_client.get_object(bucket: bucket, key: 'FOO').successful?
           return create_error 's3', "Could not fetch object from s3 bucket: #{bucket}"
         end
       end
@@ -27,7 +26,7 @@ module HealthCheck
       create_error 's3', e.message
     end
 
-    def self.s3_client
+    def self.configure_client
       return unless defined?(Rails)
 
       aws_configuration = {
@@ -40,6 +39,10 @@ module HealthCheck
       }
 
       Aws::S3::Client.new aws_configuration
+    end
+
+    def self.aws_s3_client
+      @aws_s3_client ||= configure_client
     end
 
     def self.create_error check_type, error_message
