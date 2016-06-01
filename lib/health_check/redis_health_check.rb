64d4d3ad83edc18e6e2ceefe893606c0bf2358bb
Filip <filip.haftek@airhelp.com>
2016-06-01 14:30:27 +0200

    resque, sidekiq, aws-sdk and redis gems required only if used

--- lib/health_check/redis_health_check.rb
+++ lib/health_check/redis_health_check.rb
@@ -1,10 +1,11 @@
-require 'redis'
-
 module HealthCheck
   class RedisHealthCheck
     extend BaseHealthCheck
 
     def self.check
+      if (defined?(::Redis)).nil?
+        raise "Wrong configuration. Missing 'redis' gem"
+      end
       '' if ::Redis.new.ping
     rescue Exception => e
       create_error 'redis', e.message
--- lib/health_check/sidekiq_health_check.rb
+++ lib/health_check/sidekiq_health_check.rb
@@ -1,10 +1,11 @@
-require 'sidekiq'
-
 module HealthCheck
   class SidekiqHealthCheck
     extend BaseHealthCheck
 
     def self.check
+      if (defined?(::Sidekiq)).nil?
+        raise "Wrong configuration. Missing 'sidekiq' gem"
+      end
       ::Sidekiq.redis { |r| '' if r.ping == 'PONG' }
     rescue Exception => e
       create_error 'sidekiq-redis', e.message
