899fed7c1013584bfa9925ab7e453e156b444a28
Ian Heggie <ian@heggie.biz>
2016-07-11 01:53:50 +1000

    Explicit test of redis

--- lib/health_check/resque_health_check.rb
+++ lib/health_check/resque_health_check.rb
@@ -6,7 +6,8 @@ module HealthCheck
        unless defined?(::Resque)
          raise "Wrong configuration. Missing 'resque' gem"
        end
-       '' if ::Resque.redis.ping == 'PONG'
+      res = ::Resque.redis.ping
+      res == 'PONG' ? '' : "Resque.redis.ping returned #{res.inspect} instead of PONG"
      rescue Exception => e
        create_error 'resque-redis', e.message
      end
