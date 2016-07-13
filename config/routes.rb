498fe964f8d66e277770d53d9f13fdcf5ee37fb5
cconstantine <chris@omadahealth.com>
2016-07-13 10:41:46 -0700

    Fix auto-magical addition of health_check/ route to app.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ if defined?(HealthCheck::Engine)
   unless HealthCheck::Engine.routes_already_defined
     # Ifmountable use: HealthCheck::Engine.routes.draw do
     Rails.application.routes.draw do
-      health_check_routes()
+      health_check_routes(nil, false)
     end
   end
 end
