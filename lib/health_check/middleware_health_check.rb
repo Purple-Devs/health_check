c1f8e22bd57cce19704ec13b16bb035ca27c2a9d
filiphaftek <filip.haftek@airhelp.com>
2016-10-14 14:37:16 +0200

    Merged upstream + fixed middleware (#10)

--- lib/health_check/middleware_health_check.rb
+++ lib/health_check/middleware_health_check.rb
@@ -9,39 +9,32 @@ module HealthCheck
 
     def call(env)
       uri = env['PATH_INFO']
-      uri_without_suffix = uri.gsub URI_SUFFIX_REGEX, ''
-      if uri_without_suffix == HealthCheck.uri
-        response_type = uri[/\.(json|xml)/,1] || 'plain'
-        response_method = 'response_' + response_type
-        checks = env['QUERY_STRING'][/checks=([a-z0-9\-_]*)/,1] || 'standard'
-        begin
-          errors = HealthCheck::Utils.process_checks(checks)
-        rescue => e
-          errors = e.message.blank? ? e.class.to_s : e.message.to_s
-        end
-        if errors.blank?
-          send(response_method, 200, HealthCheck.success, true)
+      if uri =~ /^\/?#{HealthCheck.uri}\/?([-_0-9a-zA-Z]*)([-_0-9a-zA-Z]*)(\.(\w*))?/
+       checks = $1 + ($1 != '' && $2 != '' ? '_' : '') + $2
+       puts checks
+       checks = 'standard' if checks == ''
+       response_type = $4
+       begin
+         errors = HealthCheck::Utils.process_checks(checks)
+       rescue => e
+         errors = e.message.blank? ? e.class.to_s : e.message.to_s
+       end
+        healthy = errors.blank?
+        msg = healthy ? HealthCheck.success : "health_check failed: #{errors}"
+        if response_type == 'xml'
+          content_type = 'text/xml'
+          msg = { healthy: healthy, message: msg }.to_xml
+        elsif response_type == 'json'
+          content_type = 'application/json'
+          msg = { healthy: healthy, message: msg }.to_json
         else
-          msg = "health_check failed: #{errors}"
-          send(response_method, 500, msg, false)
+          content_type = 'text/plain'
         end
+        [ (healthy ? 200 : 500), { 'Content-Type' => content_type }, [msg] ]
       else
         @app.call(env)
       end
     end
 
-    def response_json code, msg, healthy
-      obj = { healthy: healthy, message: msg }
-      [ code, { 'Content-Type' => 'application/json' }, [obj.to_json] ]
-    end
-
-    def response_xml code, msg, healthy
-      obj = { healthy: healthy, message: msg }
-      [ code, { 'Content-Type' => 'text/xml' }, [obj.to_xml] ]
-    end
-
-    def response_plain code, msg, healthy
-      [ code, { 'Content-Type' => 'text/plain' }, [msg] ]
-    end
   end
 end
