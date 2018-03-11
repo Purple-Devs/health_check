06f02198fe1e14fa0da650c5d724f7c0a037e66c
Ian Heggie <ian@heggie.biz>
2018-03-12 04:40:42 +1100

    Mark test migrations as level 5.0

--- test/migrate/twelve/9_create_countries.rb
+++ test/migrate/twelve/9_create_countries.rb
@@ -1,4 +1,4 @@
-class CreateCountries < ActiveRecord::Migration
+class CreateCountries < ActiveRecord::Migration[5.0]
   def self.up
     create_table :countries do |t|
       t.column :name, :string
