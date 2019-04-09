e1c7175e5dd36156daee2815e390344d8861fefd
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2019-04-09 13:54:25 +0300

    get rid of old school hash rockets syntax

--- lib/health_check/health_check_routes.rb
+++ lib/health_check/health_check_routes.rb
@@ -8,7 +8,7 @@ module ActionDispatch::Routing
 
     def add_health_check_routes(prefix = nil)
       HealthCheck.uri = prefix if prefix
-      match "#{HealthCheck.uri}(/:checks)(.:format)", :to => 'health_check/health_check#index', via: [:get, :post], :defaults => { :format => 'txt' }
+      match "#{HealthCheck.uri}(/:checks)(.:format)", to: 'health_check/health_check#index', via: [:get, :post], defaults: { format: 'txt' }
     end
 
   end
